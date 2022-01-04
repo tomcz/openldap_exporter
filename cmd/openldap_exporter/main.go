b7a0323603a48c647d90d817b7b936fbd339a14c
Marcos Lorenzo <mlorenzo@stratio.com>
2022-01-04 13:17:40 +0100

    Add TLS/StartTLS support

--- cmd/openldap_exporter/main.go
+++ cmd/openldap_exporter/main.go
@@ -17,10 +17,12 @@ import (
 
 const (
 	promAddr          = "promAddr"
-	ldapNet           = "ldapNet"
 	ldapAddr          = "ldapAddr"
+	ldapCACrt         = "ldapCACrt"
 	ldapUser          = "ldapUser"
 	ldapPass          = "ldapPass"
+	ldapSkipInsecure  = "ldapSkipInsecure"
+	ldapUseStartTLS   = "ldapUseStartTLS"
 	interval          = "interval"
 	metrics           = "metrPath"
 	jsonLog           = "jsonLog"
@@ -44,16 +46,15 @@ func main() {
 			EnvVars: []string{"METRICS_PATH"},
 		}),
 		altsrc.NewStringFlag(&cli.StringFlag{
-			Name:    ldapNet,
-			Value:   "tcp",
-			Usage:   "Network of OpenLDAP server",
-			EnvVars: []string{"LDAP_NET"},
-		}),
-		altsrc.NewStringFlag(&cli.StringFlag{
 			Name:    ldapAddr,
-			Value:   "localhost:389",
+			Value:   "ldap://localhost",
 			Usage:   "Address and port of OpenLDAP server",
 			EnvVars: []string{"LDAP_ADDR"},
+		}),
+		altsrc.NewStringFlag(&cli.StringFlag{
+			Name:    ldapCACrt,
+			Usage:   "Path to CA certificate for LDAPS (optional)",
+			EnvVars: []string{"LDAP_CA_CRT"},
 		}),
 		altsrc.NewStringFlag(&cli.StringFlag{
 			Name:    ldapUser,
@@ -64,6 +65,18 @@ func main() {
 			Name:    ldapPass,
 			Usage:   "OpenLDAP bind password (optional)",
 			EnvVars: []string{"LDAP_PASS"},
+		}),
+		altsrc.NewBoolFlag(&cli.BoolFlag{
+			Name:    ldapSkipInsecure,
+			Value:   false,
+			Usage:   "OpenLDAP Skip TLS verify (default=false)",
+			EnvVars: []string{"LDAP_SKIP_TLS_VERIFY"},
+		}),
+		altsrc.NewBoolFlag(&cli.BoolFlag{
+			Name:    ldapUseStartTLS,
+			Value:   false,
+			Usage:   "Use start TLS (optional)",
+			EnvVars: []string{"LDAP_USE_STARTTLS"},
 		}),
 		altsrc.NewDurationFlag(&cli.DurationFlag{
 			Name:    interval,
@@ -124,6 +137,30 @@ func runMain(c *cli.Context) error {
 	}
 	log.Info("service starting")
 
+	config := exporter.NewLDAPConfig()
+
+	// Process Address & TLS options
+	err := config.ProcessTLSoptions(c.String(ldapAddr),c.Bool(ldapUseStartTLS),c.Bool(ldapSkipInsecure))
+	if err != nil {
+		log.Println("Error parsing ldap address: ", err.Error())
+		os.Exit(1)
+	}
+
+	/** Load Certificate if given, and panic on error **/
+	if c.String(ldapCACrt) != "" {
+		err = config.LoadCACert(c.String(ldapCACrt))
+		if err != nil {
+			log.Println("Error loading CA certificate file: ", err.Error())
+			os.Exit(1)
+		} else {
+			log.Println("Successfully loaded CA cert file:", c.String(ldapCACrt))
+		}
+	}
+
+	config.Username = c.String(ldapUser)
+	config.Password = c.String(ldapPass)
+
+
 	server := exporter.NewMetricsServer(
 		c.String(promAddr),
 		c.String(metrics),
@@ -131,10 +168,7 @@ func runMain(c *cli.Context) error {
 	)
 
 	scraper := &exporter.Scraper{
-		Net:  c.String(ldapNet),
-		Addr: c.String(ldapAddr),
-		User: c.String(ldapUser),
-		Pass: c.String(ldapPass),
+		LDAPConfig: config,
 		Tick: c.Duration(interval),
 		Sync: c.StringSlice(replicationObject),
 	}
